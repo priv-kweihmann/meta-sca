84c35c58530e78b7f73c12b870c6d48998e39fd2
Konrad Weihmann <kweihmann@outlook.com>
2019-08-11 09:49:47 +0200

    Add missing testing recipe to script

--- recipes-sca-test/test.sh
+++ recipes-sca-test/test.sh
@@ -3,14 +3,18 @@ bitbake \
         bad-ansible \
         bad-bitbake \
         bad-css \
+        bad-go \
         bad-html \
         bad-javascript \
         bad-json \
+        bad-python-reqs \
         bad-xml \
         bad-yaml \
         busybox \
         core-image-minimal-scatest \
+        cspell \
         linux-yocto \
         openssh \
         python3-git \
+        simple-c \
         $@
