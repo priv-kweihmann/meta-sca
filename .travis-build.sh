69c7aa3d66b6e1bf11f02748113c8edd7b6edf50
Konrad Weihmann <kweihmann@outlook.com>
2019-10-20 16:06:15 +0200

    Fix travis build script
    Mind that we are running on /bin/sh

--- .travis-build.sh
+++ .travis-build.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
-timeout --foreground ${MAX_RUNTIME}m bitbake -q ${SCA_TARGET_IMG} || if [ $? == 124 ]; then echo "Timeout"; else return 1; fi;
+timeout --foreground --kill-after=${MAX_RUNTIME_KILL}m ${MAX_RUNTIME}m bitbake -q ${SCA_TARGET_IMG}
+if [ $? = 124 ]; then 
+    echo "Timeout"
+    return 0
+else 
+    return 1
+fi;
