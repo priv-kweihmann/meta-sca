2761efaa22ba862162bd76d38ac9f72ad570745c
Konrad Weihmann <kweihmann@outlook.com>
2019-10-21 21:16:39 +0200

    CI: Use travis-CI as basic CI
    - with this pipeline it's checked that
      syntax of all recipes is okay.
      It doesn't build anything

--- .travis-build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-timeout --foreground --kill-after=${MAX_RUNTIME_KILL}m ${MAX_RUNTIME}m bitbake -q ${SCA_TARGET_IMG}
-if [ $? = 124 ]; then 
-    echo "Timeout"
-    return 0
-else 
-    return 1
-fi;
