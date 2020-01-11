69cfc52400b1be044fe3a8cb280326797ef69262
Konrad Weihmann <kweihmann@outlook.com>
2020-01-11 15:23:30 +0100

    Add staging to branches script

---
+++ scripts/get_hashes.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+res=""
+for number in $@; do
+    x=$(git log --oneline --grep "Closes #${number}" --grep "Relates to #${number}" --pretty=format:"%h")
+    res="${res}\n${x}"
+done
+_BRANCHES="master zeus warrior thud"
+for b in ${_BRANCHES}; do
+    git checkout ${b} 1> /dev/null
+    git pull 1> /dev/null
+    git cherry-pick $(echo ${res} | sort | uniq | tr '\n' ' ')
+done
+
+git checkout staging
