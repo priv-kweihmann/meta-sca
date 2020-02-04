bc025649ad268da4f84cb08c5cfa64a95046a19d
Konrad Weihmann <kweihmann@outlook.com>
2020-02-04 15:11:32 +0100

    Remove unnecessary scripts from non-staging branches
    Closes #798

--- scripts/get_all_exectimes.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-## SPDX-License-Identifier: BSD-2-Clause
-## Copyright (c) 2019, Konrad Weihmann
-
-_path=$(dirname $(realpath $0))
-tasks=$(grep -Rh "addtask" ${_path}/../classes/ | cut -d' ' -f2 | sed "s/\n/ /g" | sort | uniq)
-for f in ${tasks}; do
-    ${_path}/get_exectime ${f} 2>/dev/null
-done
