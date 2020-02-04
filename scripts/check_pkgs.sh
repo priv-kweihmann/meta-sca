ff037a8f7f74855fbed8c0acea011d0232852fd7
Konrad Weihmann <kweihmann@outlook.com>
2020-02-04 20:12:29 +0100

    Remove obsolete check_pkgs script

--- scripts/check_pkgs.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/sh
-# This script checks all recipes from this layer for updates
-
-devtool check-upgrade-status $(bitbake-layers show-recipes -l meta-sca -b | grep ":$" | tr -d ':' | tr '\n' ' ') 2>&1 | grep "INFO:"
