d2393891c0e891b31d7623fba3dcd42680791120
Konrad Weihmann <kweihmann@outlook.com>
2020-02-09 16:52:55 +0100

    Don't sort commits on cherry-picking
    otherwise that could screw thing up.
    But reverse the order of commits so they are
    applied in the correct order

--- scripts/get_hashes.sh
+++ scripts/get_hashes.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 res=""
 for number in $@; do
-    x=$(git log --oneline --grep "Closes #${number}" --grep "Relates to #${number}" --pretty=format:"%h")
+    x=$(git log --oneline --grep "Closes #${number}" --grep "Relates to #${number}" --pretty=format:"%h" --reverse)
     res="${res}\n${x}"
 done
 _BRANCHES="master zeus warrior thud"
@@ -11,7 +11,7 @@ fi
 for b in ${_BRANCHES}; do
     git checkout ${b} 1> /dev/null
     git pull 1> /dev/null
-    git cherry-pick $(echo ${res} | sort | uniq | tr '\n' ' ')
+    git cherry-pick $(echo ${res} | tr '\n' ' ')
 done
 
 git checkout staging
