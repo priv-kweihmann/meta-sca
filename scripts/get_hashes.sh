92b0181be780b7d194033532b3f2fccabf61efc4
Konrad Weihmann <kweihmann@outlook.com>
2020-10-27 21:15:52 +0100

    get_hashes: update supported branches
    Signed-off-by: Konrad Weihmann <kweihmann@outlook.com>

--- scripts/get_hashes.sh
+++ scripts/get_hashes.sh
@@ -18,7 +18,7 @@ case ${mode} in
         res=$(git log origin/staging..staging --oneline | cut -d' ' -f1 | tac | tr '\n' ' ')
         ;;
 esac
-_BRANCHES="master dunfell zeus warrior"
+_BRANCHES="master gatesgarth dunfell zeus"
 if [ ! -z "${BRANCHES}" ]; then
     _BRANCHES=${BRANCHES}
 fi
